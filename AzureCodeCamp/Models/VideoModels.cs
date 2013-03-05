935664e7d1c3c3252021e833548b5470777cdc0e
Erkki Heino <evsheino@gmail.com>
2013-03-05 14:36:45 +0200

    JoukkoVideo detail nayttaa videon

--- AzureCodeCamp/Models/VideoModels.cs
+++ AzureCodeCamp/Models/VideoModels.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Globalization;
+using System.Web.Security;
 
 namespace AzureCodeCamp.Models
 {
     public class JoukkoVideo
     {
         public int ID { get; set; }
+        [Required]
         public string path { get; set; }
+        [Required]
+        public DateTime timestamp { get; set; }
+        [Required]
+        public virtual UserProfile user { get; set; }
     }
 
     public class JoukkoVideoDBContext : DbContext
